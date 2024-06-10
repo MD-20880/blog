7c0a2a2f2d16da4a8925a906b96c3f3489dcb017
Mingzhang Deng <dengmingzhang2002@outlook.com>
2024-06-10 21:44:00 +0800

    feat: dynamically load markdown from http request

--- src/components/PostContent.vue
+++ src/components/PostContent.vue
@@ -1,10 +1,25 @@
 <script setup>
-import Paragraph from './page/Paragraph.vue'
+import { ref } from 'vue';
+import MarkdownIt from 'markdown-it';
+
+const md = MarkdownIt()
+const result = ref('')
+
+let xhr = new XMLHttpRequest();
+xhr.open('GET', 'http://127.0.0.1:3000/tools/content.md',true);
+xhr.onreadystatechange = function(){
+    console.log("GET_REQUEST SEND")
+    if (xhr.readyState == 4 && xhr.status == 200){
+        result.value = md.render(xhr.responseText);
+    }
+}
+xhr.send();
+
 </script>
 
 <template>
     <div class="content">
-        <Paragraph />
+         <span v-html="result"></span>
     </div>
     <div class="footer">
         <p>
