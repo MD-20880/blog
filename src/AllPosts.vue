67b50c24ab24c0d14639fad2700af30075e86ee2
Mingzhang Deng <dengmingzhang2002@outlook.com>
2024-06-14 01:45:36 +0800

    feat: Add Allposts page content

--- src/AllPosts.vue
+++ src/AllPosts.vue
@@ -1,49 +1,48 @@
 <template>
     <div class="all-posts-inner-wrapper">
-        <PostPreview v-if="postVisibleStatus[0]" title="title1" description="description1" tags="tags1" :id="0"/>
-        <PostPreview v-if="postVisibleStatus[1]" title="title2" description="description2" tags="tags2" :id="1"/>
-        <PostPreview v-if="postVisibleStatus[2]" title="title3" description="description3" tags="tags3" :id="2"/>
-        <PostPreview v-if="postVisibleStatus[3]" title="title4" description="description4" tags="tags4" :id="3"/>
-        <PostPreview v-if="postVisibleStatus[4]" title="title5" description="description5" tags="tags5" :id="4"/>
-        <PostPreview v-if="postVisibleStatus[5]" title="title6" description="description6" tags="tags6" :id="5"/>
-        <PostPreview v-if="postVisibleStatus[6]" title="title7" description="description7" tags="tags7" :id="6"/>
-        <PostPreview v-if="postVisibleStatus[7]" title="title8" description="description8" tags="tags8" :id="7"/>
+        <span v-if="totalPosts != 0"  v-for="i in postsPerPage" :key="i">
+            <PostPreview v-if="posts.posts[(currentPage-1)*postsPerPage+i]" 
+            :title="posts.posts[(currentPage-1)*postsPerPage+i].title" 
+            :description="posts.posts[(currentPage-1)*postsPerPage+i].description" 
+            :date="posts.posts[(currentPage-1)*postsPerPage+i].date" 
+            :tags="posts.posts[(currentPage-1)*postsPerPage+i].tags" />
+        </span>
+           
         <div class="pagination">
-            <span v-for="i in totalPage">
-                <span v-if="i == currentPage" class="page-number active">{{ i }}</span>
-                <span v-else @click="setCurrentPage(i)" class="page-number">{{ i }}</span>
-            </span>
+            <button v-if="currentPage > 1" @click="currentPage--">Previous</button>
+            <span v-for=" i in totalPages" :key="i">
+                <button v-if="i == currentPage" @click="currentPage=i" :class="{active: i == currentPage}">{{ i }}</button>
+                <button v-else @click="currentPage=i">{{ i }}</button>
+            </span> <!-- Display page numbers -->
+            <button v-if="currentPage < totalPages" @click="currentPage++">Next</button>
         </div>
     </div>
 </template>
 
 
 <script setup>
-import {ref} from 'vue'
+import { onMounted, ref , computed} from 'vue'
 import PostPreview from './components/PostPreview.vue'
 
-const totalPosts = 8
-const totalPage = 2
-const postPerPage = 5
-const postVisibleStatus = ref([])
-const curentStart = ref(0)
-const currentPage = ref(0)
-let list = Array.from({length: postPerPage}, (_, i) => true)
-postVisibleStatus.value = list
-console.log(postVisibleStatus.value)
+const posts = ref({})
+const currentPage = ref(1)
+const postsPerPage = 5
+const totalPosts = ref(0)
+const totalPages = ref(0)
 
-const setCurrentPage = (page) => {
-    currentPage.value = page
-    curentStart.value = (page-1)*postPerPage
-    for (let i = 0; i < totalPosts; i++){
-        postVisibleStatus.value[i] = false
-    }
-    for (let i = curentStart.value; i < curentStart.value+postPerPage; i++){
-        postVisibleStatus.value[i] = true
-    }
-    console.log(page)
-    console.log(postVisibleStatus.value)
-}
+
+onMounted(() => {
+    fetch(`${window.location.origin}/posts.json`)
+        .then(response => response.json())
+        .then(data => {
+            posts.value=data
+            totalPages.value = Math.ceil(posts.value.posts.length/postsPerPage)
+            totalPosts.value = posts.value.posts.length
+            console.log(posts.value)    
+        })
+})
+
+
 
 
 //Load all posts
---
+++ src/components/PostPreview.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="post-preview">
+        <div class="post-preview-inner-wrapper">
+            <div class="post-preview-content">
+                <h2 class="post-preview-title">{{ title }}</h2>
+                <p class="post-preview-description">{{ description }}</p>
+                <div class="post-preview-tags">
+                    <span v-html="tags"></span>
+                </div>
+                <p class="post-preview-date">{{ date }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+const props = defineProps(['title','description','date','tags'])
+
+</script>
